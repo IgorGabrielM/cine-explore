d261fe5c5faeb6ba95cd322cbdd28fd75a320153
igorGabrielM <igorgabprofissional@gmail.com>
2023-12-30 23:17:38 -0300

    feat: implement test of movie card

---
+++ src/app/components/tests/movieCard.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MovieCard from '../MovieCard';
+
+test('Verifica se o link está funcionando corretamente', () => {
+    // Mock do objeto movie
+    const movie = {
+        movie: {
+            id: '278',
+            poster_path: '/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
+            title: 'The Shawshank Redemption',
+            vote_average: 8.708,
+        },
+    };
+
+    render(<MovieCard
+        {...movie} />);
+
+    const linkElement = screen.getByTestId('movie-card');
+
+    expect(linkElement).toHaveAttribute('href', 'pages/movie/278');
+
+    fireEvent.click(linkElement);
+});
